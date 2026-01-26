a657737cc35b1291950539d0112a71efe443b900
saad-aziz1 <saadaziz10000@gmail.com>
2026-01-26 20:12:13 +0500

    Add JWT token logic with cookie parser

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,3 +1,4 @@
+import generateToken from "../config/token.js"
 import User from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
 
@@ -21,6 +22,16 @@ export const signUp = async (req, res) => {
             password: hashPassword,
             userName
         })
+
+        const token = generateToken(newUser._id)
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV == "production",
+            sameSite: "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
         return res.status(201).json({
             message: "User register successfully",
             user: {
@@ -31,7 +42,7 @@ export const signUp = async (req, res) => {
             }
         })
 
-    } catch (error) {
+        } catch (error) {
         console.log(error);
         return res.status(500).json({
             message: "Internal Server error",
--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDb from './config/DBConnected.js';
 import authRouter from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api", authRouter)
 
 const port = process.env.PORT
